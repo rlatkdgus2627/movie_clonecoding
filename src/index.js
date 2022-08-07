6f683529d77cbecbdf6a6226b51ad4fcc1ae4ffd
김상현 <rlatkdgus2627@naver.com>
2022-08-07 06:50:58 +0000

    2022/8/7 16시

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+//첫 번째 인자 App 컴포넌트가 반환하는 것들을 화면에 그림
+//두 번째 인자 App 컴포넌트가 그려질 위치
